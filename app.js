a09b88bac14d8d3f4a370ea8cb5188aea8f19cff
Rambod Goshtasbi <rpg225@gmail.com>
2024-05-31 16:09:04 -0400

    Added event listeners and styling

--- app.js
+++ app.js
@@ -5,10 +5,50 @@ const infoDisplay = document.querySelector("#info")
 king.addEventListener("drag", dragging )
 king.addEventListener("dragstart", dragStart)
 
-function dragging (e) {
-    console.log(e + "is being dragged")
-}
+squares.forEach(square => {
+    square.addEventListener("dragover", dragOver)        
+    square.addEventListener("dragenter", dragEnter)
+    square.addEventListener("dragleave", dragLeave)
+    square.addEventListener("drop", dragDrop)
+    square.addEventListener("dragend", dragEnd)
+})
+
+let beingDragged
 
 function dragStart (e) {
-    console.log("dragging has started on " + e.target)
+    beingDragged = e.target
+    console.log("dragging has started on " + beingDragged.id)
+}
+
+
+function dragging () {
+    console.log("You are dragging a " + beingDragged.id)
+    infoDisplay.textContent = "You are dragging a " + beingDragged.id
+}
+
+function dragOver (e) {
+    e.preventDefault()
+    console.log("You are dragging something over" + e.target.classList)
+}
+
+function dragEnter (e) {
+    console.log("You are entering the space of " + e.target.classList)
+    e.target.classList.add("highlight")
+}
+
+function dragLeave (e) {
+    console.log("You are leaving the space of " + e.target.classList)
+    e.target.classList.remove("highlight")
+}
+
+function dragDrop (e) {
+    e.target.append(beingDragged)
+    e.target.classList.remove("highlight")
+}
+
+function dragEnd (e) {
+    e.target.classList.add("target")
+    setTimeout(() => e.target.classList.remove("target"), 100)
+    console.log("The drag has ended in " + e.target.classList)
+    infoDisplay.textContent = ""
 }
